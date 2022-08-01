1e63bfe9e6a77d1e66dcad154c0502ea4f905687
arbaaz-77 <43441056+arbaaz-77@users.noreply.github.com>
2022-08-01 17:54:07 +0300

    fixed style clash

--- src/components/CategoryPreview/CategoryPreview.jsx
+++ src/components/CategoryPreview/CategoryPreview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import ProductCard from "../ProductCard/ProductCard";
 
 import "./CategoryPreview.scss";
@@ -7,7 +7,9 @@ const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className="preview">
         {products
